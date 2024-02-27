5ac0aa92a5ce726aa14fef966ebe2ea50ce1656e
Tenzin choyang <choyangt9@gmail.com>
2024-02-27 15:29:49 +0530

    first repostary

---
+++ div.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int first, second;
+    float divide;
+
+    printf("Enter the first number: ");
+    scanf("%d", &first);
+
+    printf("Enter the second number: ");
+    scanf("%d", &second);
+
+    divide = (float)first / second;
+
+    printf("Result of division: %.2f\n", divide);
+
+    return 0;
+}
